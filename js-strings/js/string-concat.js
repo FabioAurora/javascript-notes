b415d9a20ffe647ab5ebd1470b89598c3ef0cd0f
Fabio Aurora <fabioauroradev@gmail.com>
2022-02-26 18:50:15 +0000

    concat() method

---
+++ js-strings/js/string-concat.js
@@ -0,0 +1,8 @@
+// concat() joins two or more strings:
+let text1 = "Hello";
+let text2 = "World";
+let text3 = text1.concat("", text2);
+
+// NOTE:
+// All string methods return a new string. They don't modify the original string.
+// gs are immutable: Strings cannot be changed, only replaced.
