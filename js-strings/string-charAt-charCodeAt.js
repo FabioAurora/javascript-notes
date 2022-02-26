13782958cbb100f159faf70581a11e560969dbf0
Fabio Aurora <fabioauroradev@gmail.com>
2022-02-26 19:05:18 +0000

    charAt() & charCodeAt() methods

---
+++ js-strings/string-charAt-charCodeAt.js
@@ -0,0 +1,7 @@
+// The charAt() method returns the character at a specified index (position) in a string:
+let text = "HELLO WORLD";
+let char = text.charAt(0); // - H
+
+// The charCodeAt() method returns the unicode of the character at a specified index in a string:
+let text1 = "HELLO WORLD";
+let char1 = text.charCodeAt(0);
