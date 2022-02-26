b423f17018123c37abc281e64a58ea21dab531fd
Fabio Aurora <fabioauroradev@gmail.com>
2022-02-26 18:55:38 +0000

    trim() method

---
+++ js-strings/js/string-trim.js
@@ -0,0 +1,7 @@
+// The trim() method removes whitespace from both sides of a string:
+let text1 = "      Hello World!      ";
+let text2 = text1.trim();
+
+console.log(
+  `text1 length is ${text1.length}, and text2 length is ${text2.length}`
+);
