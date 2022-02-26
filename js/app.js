0a0203635ddc20308b3b2fc96f9a1173c248793e
Fabio Aurora <fabioauroradev@gmail.com>
2022-02-26 15:07:45 +0000

    concatenation using "+" operator

--- js/app.js
+++ js/app.js
@@ -2,5 +2,12 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
   const name = prompt("What is your name?");
-  alert(`Hello ${name}, nice to see you!`); // Concatenating string using back ticks (``)
+  alert(`Hello ${name}, nice to see you!`); // Concatenating string using backtick character (``)
 });
+
+/* Concatenation using "+" operator */
+const greeting = "Hello";
+const name = "Fabio";
+console.log(greeting + ", " + name); // using "+"operator
+
+console.log(`${greeting} ${name}`); // template literals usually give you more readable code
