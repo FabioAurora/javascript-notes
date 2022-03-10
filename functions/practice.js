0d228cf9e54c1b893e401f999b4c0f7c61fd4c33
Fabio Aurora <fabioauroradev@gmail.com>
2022-03-10 20:47:47 +0000

    completed practice.js exercises.

--- functions/practice.js
+++ functions/practice.js
@@ -1,3 +1,9 @@
+/* Write a function called add7 that takes one number and returns that number + 7.
+Write a function called multiply that takes 2 numbers and returns their product.
+Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+Write a function called lastLetter that takes a string and returns the very last letter of that string:
+lastLetter("abcd") should return "d" */
+
 // 1----------------------------------
 function add7(num) {
   return num + 7;
@@ -5,20 +11,25 @@ function add7(num) {
 
 console.log(add7(10));
 
-//2--------------------------
-
+// 2----------------------------------
 function multiply(x, y) {
   return x * y;
 }
 
 console.log(multiply(2, 2));
 
-//3---------------------------------------
+// 3-------------------------------------
 function capitalize(string) {
-  let capLetter = string.slice(0, 1).toUpperCase();
-  let restString = string.slice(1).toLowerCase();
-
-  console.log(`${capLetter}${restString}`);
+  const capLetter = string.slice(0, 1).toUpperCase();
+  const lowerLetters = string.slice(1).toLowerCase();
+  return `${capLetter}${lowerLetters}`;
 }
 
-capitalize("FABIO");
+console.log(capitalize("FABIO"));
+
+// 4------------------------------------------
+function lastLetter(string) {
+  return string.slice(-1);
+}
+
+console.log(lastLetter("abcd"));
